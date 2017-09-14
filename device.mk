52a7a863bc7af1e6dd3f460719c2757f488411ed
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    hiae: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb

--- device.mk
+++ device.mk
@@ -226,6 +226,11 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     librmnetctl
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
