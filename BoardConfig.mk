de73760b4292085305abc19524268f43ec59e8e1
Ethan Chen <intervigil@gmail.com>
2017-10-01 20:30:31 -0700

    hiae: Use separate fstab for recovery
    * otapackage requires /system to be present in the
      fstab, but early mounting /system means that it
      cannot be in the Android fstab, otherwise fs_mgr
      will fail to mount it and subsequently fail to boot.
    
    Change-Id: I0e90df543409fcc94bf4f97c5cb6e9b2fd12906c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,7 +129,7 @@ BOARD_USES_QC_TIME_SERVICES := true
 TARGET_USE_SDCLANG := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/recovery.fstab
 
 # TWRP
 ifeq ($(RECOVERY_VARIANT),twrp)
