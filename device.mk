cb9cc1f859dfda7b1d513cb29c05c7f908b35ff7
Ethan Chen <intervigil@gmail.com>
2017-10-11 20:53:37 -0700

    hiae: Use Lineage legacy camera provider
    Change-Id: I73e072e70d89c43da0e4eb7bf8b53757e0c15a85

--- device.mk
+++ device.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
     camera.msm8952 \
     Snap
 
