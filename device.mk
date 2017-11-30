329727f96f415a4f7c9b9db2156959ebb5322120
TheScarastic <warabhishek@gmail.com>
2017-11-30 18:31:58 +0530

    mido: Add custom biometrics
     * Goodix hal uses old fingeprintd interface to send binder calls
     * Use new biometrics binder calls for fpc
    
    co-authored with:- alexstsv2
    
    Change-Id: Ida433860d9f05b652744c9f91e11afe6a5966b87

--- device.mk
+++ device.mk
@@ -127,7 +127,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service.tulip
 
 # Gesture Handler
 PRODUCT_PACKAGES += \
