4d271f25a8ca3680353a31c8625514dc1a42e1dc
Tom Marshall <tdm.code@gmail.com>
2018-02-09 00:56:02 +0100

    tulip: Update camera wrapper for O
    Change-Id: I9ff7f90dcc787ac792c49a96a52cdbce1c2f3c13

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,13 +7,18 @@ LOCAL_SRC_FILES := CameraWrapper.cpp
 LOCAL_C_INCLUDES := \
     system/media/camera/include
 
+LOCAL_STATIC_LIBRARIES := \
+    libbase libarect
+
 LOCAL_HEADER_LIBRARIES += \
     libnativebase_headers
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libdl
+    libhardware liblog libcamera_client libutils libcutils libdl \
+    android.hidl.token@1.0-utils \
+    android.hardware.graphics.bufferqueue@1.0
 
-#LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_CFLAGS := -Werror
