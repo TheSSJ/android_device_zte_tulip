f0f9070b6def560e93b15d09ac3f2cc4b0457682
TheSSJ <greg.house.ssj@gmail.com>
2019-05-04 21:37:54 +0000

    camera: revert to original status before los16
    Change-Id: I172edaea29abef6499a8aeec79125f5d2f9abdf2

--- device.mk
+++ device.mk
@@ -116,11 +116,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
     camera.msm8952 \
     libshim_camera \
     Snap
