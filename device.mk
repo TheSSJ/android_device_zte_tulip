77e5cf16b6b5c619761fd6d4d1c502a7e2e7315b
TheSSJ <greg.house.ssj@gmail.com>
2019-05-04 21:04:12 +0000

    disable netd for now
    Change-Id: Ib58112814c679dd9eb6c8cb81aadb3bd90d46865

--- device.mk
+++ device.mk
@@ -247,8 +247,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
 # Net
-PRODUCT_PACKAGES += \
-    android.system.net.netd@1.0
+#PRODUCT_PACKAGES += \
+#    android.system.net.netd@1.0
 
 # NFC
 PRODUCT_PACKAGES += \
