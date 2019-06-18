a5916993bc9ea74ea629a81a6d2de1c417b632ad
TheSSJ <greg.house.ssj@gmail.com>
2019-06-18 11:39:05 +0000

    Biometrics: fix up service version to include
    Change-Id: I6561587a95e48a94f6ad5bfbefbaf7cb83546456

--- device.mk
+++ device.mk
@@ -158,7 +158,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.tulip \
+    android.hardware.biometrics.fingerprint@2.0-service.tulip \
     fakelogprint
 
 # Gesture Handler
